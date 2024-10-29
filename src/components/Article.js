1688d289cd532f40615ba4a3632bdcc90a5863cd
jobwayne <otienojobwayneotieno@gmail.com>
2024-10-29 21:13:28 +0300

    react

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-function Article() {
+function Article(props) {
   return (
-    <div>
-      {/* please pass this test */}
-    </div>
-  );
+    
+     <h1>hello from the Header!</h1>);
+  
 }
 
 export default Article;
+
